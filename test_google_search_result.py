44767f8936612838faea061e355e34ac58db7bd8
Глеб Троц <gntrots@mts.ru>
2024-11-22 14:17:26 +0500

    added homework for lesson 3

---
+++ test_google_search_result.py
@@ -0,0 +1,15 @@
+from selene import browser, be, have
+from fixtures import browser_setup
+
+
+def test_google_found(browser_setup):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    (browser.element('[id="rcnt"]').should(be.visible)
+     .should(have.text('User-oriented Web UI browser tests in Python')))
+
+
+def test_google_not_found():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('fsdnfusi23uniufgbirbuei').press_enter()
+    browser.element('[id="rcnt"]').should(be.visible).should(have.text('ничего не найдено'))

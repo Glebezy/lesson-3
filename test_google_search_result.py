14720ce057184dbaadb2d3d1f5bde9f314e52d95
Глеб Троц <gntrots@mts.ru>
2024-11-24 17:54:01 +0500

    edited open url in tests

--- test_google_search_result.py
+++ test_google_search_result.py
@@ -3,13 +3,13 @@ from fixtures import browser_setup
 
 
 def test_google_found(browser_setup):
-    browser.open('https://google.com')
+    browser.open('https://google.com/ncr')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     (browser.element('[id="rcnt"]').should(be.visible)
      .should(have.text('User-oriented Web UI browser tests in Python')))
 
 
 def test_google_not_found():
-    browser.open('https://google.com')
+    browser.open('https://google.com/ncr')
     browser.element('[name="q"]').should(be.blank).type('fsdnfusi23uniufgbirbuei').press_enter()
-    browser.element('[id="rcnt"]').should(be.visible).should(have.text('ничего не найдено'))
+    browser.element('[id="rcnt"]').should(be.visible).should(have.text('did not match any documents'))
